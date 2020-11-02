9e20ded5ee88d96c9314b6f331dca14345da1715
omantel <oliviermantel@gmail.com>
2020-11-02 11:41:32 +0100

    Add text animation

--- random-quotes/src/MyApp.js
+++ random-quotes/src/MyApp.js
@@ -12,24 +12,33 @@ class Quotes extends React.Component {
     constructor(props){
         super(props)
         this.state = {
-            ...quoteList[0]
+            ...quoteList[0],
+            animationClass: false
         }
         this.handleClick = this.handleClick.bind(this);
     }
     handleClick(){
+
         let randomNumber = Math.floor(Math.random() * Math.floor(quoteList.length));
         let newQuote = quoteList[randomNumber];
+
         this.setState({
-            ...newQuote
+            ...newQuote,
+            animationClass: true,
         })        
+        setTimeout(()=> {
+            this.setState({
+                animationClass: false
+            })
+        }, 200)
     }
     render() {
       return (
         <div id="MyApp" className="container">
             <h1 className="title">Random Quotes</h1>
             <div id = "quote-box" className="quote-box"> 
-                <p id="text" className="text">"{this.state.quote}"</p>
-                <p id="author" className="author"> <i>{this.state.author}</i> </p>    
+                <p id="text" className={'text ' + (this.state.animationClass? "animation-opacity" : "") } >"{this.state.quote}"</p>
+                <p id="author" className={'author ' + (this.state.animationClass? "animation-opacity" : "") }> <i>{this.state.author}</i> </p>    
                 <div className="links">
                     <Share />
                     <button  id="new-quote" className="new-quote  btn" onClick={this.handleClick}>New quote</button>
